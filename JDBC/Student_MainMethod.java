6e88ee0f656774dda38b3690bc6f6fcdec4d7bdd
Shruti Dhere <dhereshruti351@gmail.com>
2026-04-17 13:17:52 +0530

    Add files via upload

---
+++ JDBC/Student_MainMethod.java
@@ -0,0 +1,11 @@
+package jdbc;
+
+public class Student_MainMethod {
+
+	public static void main(String[] args) throws Exception {
+		Student s = new Student();
+		s.insertData();
+	s.deleteData();
+//		s.updateData();
+	}
+}
